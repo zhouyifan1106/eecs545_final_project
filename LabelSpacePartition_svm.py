89bba29cfe9978cecaeb26de9b71a57868ed2fc9
NaichenNi <ninaichen0220@gmail.com>
2018-12-14 14:39:50 -0500

    Label Space Partition with SVM

---
+++ LabelSpacePartition_svm.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 12 20:14:19 2018
+
+@author: naichen
+"""
+
+import LabelPowerset_svm as lp_svm
+import numpy as np
+from scipy.sparse import csc_matrix
+from scipy.sparse import hstack
+
+
+def fit(X, trans_result,cluster):
+    classifiers = []
+    for i in range(len(cluster)):
+        classifier = lp_svm.fit(X, trans_result[i])
+        classifiers.append(classifier)
+    return classifiers
+
+#def convert_X(mat):
+#    mat = csr_matrix(mat)
+#    return mat
+
+def predict(classifiers, X, trans_result):
+    new_X = X    
+    predictions = [csc_matrix(np.array(lp_svm.predict(classifiers[i], new_X, trans_result[i]))) for i in range(len(classifiers))]
+    return hstack(predictions)
+
