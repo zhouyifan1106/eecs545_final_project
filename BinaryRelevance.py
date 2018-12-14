915f6f8a71defe8041c579de76f3bb3e819f9000
Tianqi Lu <lu_tianqi@berkeley.edu>
2018-12-14 16:00:44 -0500

    Binary Relevance
    Binary Relevance algorithm

--- BinaryRelevance.py
+++ BinaryRelevance.py
@@ -6,14 +6,12 @@ Created on Tue Dec 11 18:16:04 2018
 @author: TLuv
 """
 
-import copy
 import numpy as np
-from scipy.sparse import hstack, issparse, lil_matrix
-import scipy.sparse as sp
+from scipy.sparse import hstack
 from scipy.sparse import csr_matrix, csc_matrix
-from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
+import copy
 from sklearn.linear_model import LogisticRegression
+
 
 ###################################### Binary Relevance ########################################
 def convert_X(mat):
@@ -29,20 +27,9 @@ def fit(X, y):
     return classifiers
 
 def predict(classifiers, X):
-    #new_X = convert_X(X)   
     predictions = [csc_matrix(np.array(classifier.predict(X))).T for classifier in classifiers]
     return hstack(predictions)
 
-
-#np.random.seed(0)
-#X = np.random.randn(1000, 200)
-#y = np.random.randn(1000, 20)
-#y[y <= 0] = 0
-#y[y > 0 ] = 1
-#
-#br = fit(X, y)
-#pred = predict(br, X)
-#pred2 = pred.toarray()
 
 
     
